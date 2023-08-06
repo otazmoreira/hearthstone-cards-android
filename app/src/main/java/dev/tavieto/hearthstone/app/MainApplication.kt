2414e0960a0c66f34036720447b1b89e8e5ce3d4
Otávio Gabriel <tavieto@outlook.com>
2023-08-06 15:00:00 -0300

    delete todo

--- app/src/main/java/dev/tavieto/hearthstone/app/MainApplication.kt
+++ app/src/main/java/dev/tavieto/hearthstone/app/MainApplication.kt
@@ -9,7 +9,6 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 
-// TODO - adicionar tela de splash para verificar se o app está fora do ar
 class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
